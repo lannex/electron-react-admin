5d13333a81dd94372ccb60b1eef5165651815592
Jimseung <lannex.shin@gmail.com>
2020-03-26 12:34:19 +0900

    Setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import "../styles/app.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Electron!</h1>
+        <p>Electron React Admin</p>
+      </div>
+    );
+  }
+}
+
+export default App;
