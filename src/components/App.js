32831fc6f586d43f177b652a5d445d4581411b1c
Jimseung <lannex.shin@gmail.com>
2020-03-26 13:10:59 +0900

    Setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
-import "../styles/app.css";
-import React, { Component } from "react";
+import '../styles/app.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, Electron!</h1>
-        <p>Electron React Admin</p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Electron!</h1>
+      <p>Electron React Admin</p>
+    </div>
+  );
+};
 
 export default App;
